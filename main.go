1f02c94570a3f6abaf936112ed46331af3d72301
Brian Sorahan <bsorahan@gmail.com>
2017-03-26 11:21:17 -0500

    use github.com/scgolang/sampler package for sample playback

--- main.go
+++ main.go
@@ -14,18 +14,18 @@ func main() {
 		deviceID     string
 		initialTempo float64
 		samplesDir   string
+		scsynthAddr  string
 	)
 	flag.Float64Var(&initialTempo, "t", float64(120), "tempo")
 	flag.StringVar(&deviceID, "device", "hw:0,0,0", "System-specific MIDI device ID")
 	flag.StringVar(&samplesDir, "samples", "samples", "samples directory")
+	flag.StringVar(&scsynthAddr, "scsynth", "127.0.0.1:57120", "scsynth UDP listening address")
 	flag.Parse()
 
-	samples, samplesErr := NewSamples(ctx, samplesDir)
+	samples, samplesErr := NewSamples(samplesDir, scsynthAddr)
 	if samplesErr != nil {
 		log.Fatal(samplesErr)
 	}
-	<-samples.LoadedChan
-
 	pad, padErr := OpenLaunchpad(ctx, deviceID, samples.SampleChan, initialTempo)
 	if padErr != nil {
 		log.Fatal(padErr)
@@ -33,7 +33,7 @@ func main() {
 	defer func() { _ = pad.Close() }() // Best effort.
 
 	go func() {
-		if err := samples.Main(); err != nil {
+		if err := samples.Main(context.Background()); err != nil {
 			log.Fatal(err)
 		}
 	}()
--- samples.go
+++ samples.go
@@ -4,72 +4,39 @@ import (
 	"context"
 	"log"
 	"path/filepath"
-	"time"
 
 	"github.com/pkg/errors"
-	"github.com/scgolang/sc"
-	"golang.org/x/sync/errgroup"
+	"github.com/scgolang/sampler"
 )
 
 // Samples plays back samples.
 type Samples struct {
-	*errgroup.Group
+	*sampler.Sampler
 
 	LoadedChan chan struct{}
 	SampleChan chan int
 
-	ctx context.Context
 	dir string
-	sc  *sc.Client
-	scg *sc.Group
 }
 
 // NewSamples creates a new sample player.
-func NewSamples(ctx context.Context, dir string) (*Samples, error) {
-	client, err := sc.NewClient("udp", "127.0.0.1:0", "127.0.0.1:57120", 5*time.Second)
+func NewSamples(dir, scsynthAddr string) (*Samples, error) {
+	samp, err := sampler.New(scsynthAddr)
 	if err != nil {
-		return nil, errors.Wrap(err, "creating SuperCollider client")
+		return nil, err
 	}
-	scgroup, err := client.AddDefaultGroup()
-	if err != nil {
-		return nil, errors.Wrap(err, "adding default group")
-	}
-	g, gctx := errgroup.WithContext(ctx)
-
 	s := &Samples{
-		Group:      g,
+		Sampler:    samp,
 		LoadedChan: make(chan struct{}),
-		SampleChan: make(chan int, NumTracks*NumTracks),
-		ctx:        gctx,
+		SampleChan: make(chan int),
 		dir:        dir,
-		sc:         client,
-		scg:        scgroup,
 	}
-	if err := s.sc.SendDef(def); err != nil {
-		return nil, errors.Wrap(err, "sending synthdef")
-	}
-	log.Println("sent synthdef")
-
 	if err := s.loadSamples(); err != nil {
 		return nil, errors.Wrap(err, "loading samples")
 	}
 	log.Println("loaded samples")
 
 	return s, nil
-}
-
-// load loads a single sample
-func (s *Samples) load(bufnum int32, sample string) error {
-	samplePath, err := filepath.Abs(sample)
-	if err != nil {
-		return errors.Wrap(err, "making path absolute")
-	}
-	if _, err := s.sc.ReadBuffer(samplePath, bufnum); err != nil {
-		return errors.Wrap(err, "reading buffer")
-	}
-	log.Printf("loaded %s\n", samplePath)
-
-	return nil
 }
 
 // loadSamples loads all the wav files in the current directory.
@@ -85,46 +52,19 @@ func (s *Samples) loadSamples() error {
 		if i >= NumTracks*NumBanks {
 			break
 		}
-		if err := s.load(int32(i), sample); err != nil {
-			return err
+		if err := s.Add(i, sample); err != nil {
+			return errors.Wrap(err, "adding sample")
 		}
 	}
-	close(s.LoadedChan)
 	return nil
 }
 
 // Main is the main loop of the sample player.
-func (s *Samples) Main() error {
-	s.Go(s.play)
-	return s.Wait()
-}
-
-// play is an infinite loop that plays samples
-func (s *Samples) play() error {
+func (s *Samples) Main(ctx context.Context) error {
 	for bufnum := range s.SampleChan {
-		var (
-			action = sc.AddToTail
-			ctls   = map[string]float32{
-				"bufnum": float32(bufnum),
-			}
-			sid = s.sc.NextSynthID()
-		)
-		if _, err := s.scg.Synth("beats_def", sid, action, ctls); err != nil {
+		if err := s.Play(bufnum, nil); err != nil {
 			return err
 		}
 	}
 	return nil
 }
-
-var def = sc.NewSynthdef("beats_def", func(params sc.Params) sc.Ugen {
-	sig := sc.PlayBuf{
-		NumChannels: 1,
-		BufNum:      params.Add("bufnum", 0),
-		Done:        sc.FreeEnclosing,
-	}.Rate(sc.AR)
-
-	return sc.Out{
-		Bus:      sc.C(0),
-		Channels: sc.Multi(sig, sig),
-	}.Rate(sc.AR)
-})
